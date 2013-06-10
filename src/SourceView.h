6c69a879fbd64576b41d385cff314de2ef5792bd
Sebastien Metrot <meeloo@meeloo.net>
2013-06-10 10:47:04 +0200

    added basic SourceCode view

---
+++ src/SourceView.h
@@ -0,0 +1,35 @@
+//
+//  SourceView.h
+//  Noodlz
+//
+//  Created by Sébastien Métrot on 6/10/13.
+//
+//
+
+#pragma once
+
+#include "nui.h"
+
+class SourceView : public nuiSimpleContainer
+{
+public:
+  SourceView();
+  virtual ~SourceView();
+
+  bool Load(const nglPath& rPath);
+  void ShowText(int line, int col);
+
+  void AddLine(const nglString& rString);
+  virtual nuiRect CalcIdealSize();
+  virtual bool SetRect(const nuiRect& rRect);
+
+  virtual bool Draw(nuiDrawContext* pContext);
+
+  virtual bool Clear();
+private:
+
+  nuiRect GetSelectionRect();
+  std::vector<nuiLabel*> mLines;
+  int32 mLine;
+  int32 mCol;
+};
